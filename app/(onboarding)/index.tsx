08e04958f5a0f9a154c5c8a54e3cd19cd658c60e
bobcermak <bohuslavcermak62@gmail.com>
2026-04-21 15:56:09 +0200

    feat: implement onboarding welcome screen and update project documentation

--- app/(onboarding)/index.tsx
+++ app/(onboarding)/index.tsx
@@ -8,7 +8,7 @@ const WelcomeScreen = () => {
 
   return (
     <View className="flex-1 mt-[200px] justify-end">
-      <View className="w-full items-center justify-center relative -top-[7%]">
+      <View className="w-full items-center justify-center relative -top-[12.5%]">
         <FloatingImage
           source={require("@/assets/images/yumi-welcome.png")}
           resizeMode="cover"
@@ -55,12 +55,12 @@ const WelcomeScreen = () => {
           }}
         />
       </View>
-      <View className="bg-dark p-6 py-12 rounded-t-[40px] shadow-xl gap-[1rem]">
-        <View>
-          <Text className="title text-5xl text-center font-nunito-800 text-white leading-[44px] pt-2">Welcome To <Text className="text-yellow">Yumi</Text> App</Text>
-          <Text className="base-text text-center text-white/50 mt-2">Skip the search. <Text className="font-nunito-700 text-yellow">Yumi's AI scans</Text> and identifies your food instantly. Your camera is now your <Text className="font-nunito-700 text-pink">nutritionist</Text>.</Text>
+      <View className="bg-dark p-8 py-16 rounded-t-[40px] shadow-xl gap-[2rem]">
+        <View className="max-w-[340px]">
+          <Text className="title text-5xl text-center font-nunito-800 text-white leading-[44px] pt-2">Welcome to <Text className="text-yellow">Yumi</Text> app</Text>
+          <Text className="base-text text-center text-white/50 mt-3">Skip the search. <Text className="font-nunito-700 text-yellow">Yumi's AI scans</Text> and identifies your food instantly. Your camera is now your <Text className="font-nunito-700 text-pink">nutritionist</Text>.</Text>
         </View>
-        <Button className="rounded-[30px] mx-0 w-full py-5 px-1" textClassName="text-xl" onPress={() => router.push("/user-information")}>
+        <Button className="rounded-[30px] mx-0 w-full py-5 px-2" textClassName="text-xl" onPress={() => router.push("/user-information")}>
           Get Started
         </Button>
         <TouchableOpacity>
